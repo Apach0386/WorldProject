ed4fa97ed1e90abe67715b9139824e967ce4af9b
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2023-11-13 10:21:54 +0200

    update organism

--- Organism.cs
+++ Organism.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace WorldProject;
 
-namespace WorldProject
+internal abstract class Organism
 {
-    internal class Organism
-    {
-    }
+    public int Power { get; set; } = 0;
+    public int Init { get; set; } = 0;
+
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+
+    public abstract void Action();
+
+    public abstract void Collision();
+
+    public abstract void Draw();
+
 }
