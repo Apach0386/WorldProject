91d5a9e0f00b375074c1bbdc4243c3ef138c6c93
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2023-11-13 09:48:47 +0200

    add world

---
+++ World.cs
@@ -0,0 +1,21 @@
+﻿namespace WorldProject;
+
+internal class World
+{
+    private int width = 0;
+    private int height = 0;
+    public World(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public void ExecuteTure()
+    {
+
+    }
+
+    public void DrawWorld()
+    {
+
+    }
+}
