cd368f92af2a9b2397f8e081be7de047ff9324fc
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2023-11-16 09:16:46 +0200

    add kosylya and some logick

--- Plants/Kosylya.cs
+++ Plants/Kosylya.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace WorldProject.Plants;
 
-namespace WorldProject.Plants
+internal class Kosylya : Plant
 {
-    internal class Kosylya
+    public Kosylya(int x, int y) : base(x, y)
     {
+    }
+
+    public override void Action(World world)
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            int probability = Random.Shared.Next(1, 4);
+
+            int xR = Random.Shared.Next(-1, 1);
+            int yR = Random.Shared.Next(-1, 1);
+
+            if (probability == 1)
+            {
+                Kosylya k = new Kosylya(X + xR, Y + yR);
+                world.AddOrganism(this);
+            }
+
+        }
+    }
+
+    public override void Collision()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine('K');
     }
 }
